9f37f644a70ace8403a8e2a23bd234e9802fda96
peter-samir-luis <68796155+peter-samir-luis@users.noreply.github.com>
2021-07-25 21:05:40 +0200

    Add files via upload

---
+++ final_cal_total_distance.h
@@ -0,0 +1,9 @@
+#include "stdint.h"
+#include <math.h>
+#define  pi 3.14159265358979323846        
+
+//constant pi which is equal 22/7
+
+double convert_deg2rad(double degree);                                                   // function which converts from degree to radian 
+double convert_rad2deg(double radian);                                                  // function which converts from radian  to degree
+double cal_distance(double latitude_1, double longitude_1, double latitude_2, double longitude_2);
