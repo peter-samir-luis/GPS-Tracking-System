3605d42d193a4090d809e1278c88d5f47ca4ac1c
peter-samir-luis <68796155+peter-samir-luis@users.noreply.github.com>
2021-07-25 21:09:29 +0200

    Add files via upload

---
+++ LCD.h
@@ -0,0 +1,8 @@
+#include "tm4c123gh6pm.h" 
+
+void delay(int n);
+void LCD_command(char x);
+void LCD_DATA(char y);
+void LCD_display_string(char* mystring);
+void LCD_number(int x) ;
+void intial_LCD(void);
